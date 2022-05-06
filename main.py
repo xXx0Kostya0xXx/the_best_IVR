bc7571e3102c7b0d3aac8e4416696e6f991c35ba
xXx0Kostya0xXx <102747508+xXx0Kostya0xXx@users.noreply.github.com>
2022-05-06 11:06:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from PIL import Image
-from functools import reduce
 import numpy as np
-
-ORIGIN = np.array([0, 0, 0, 0])
-DEFAULT_OBJ_COLOR = 255
-DEFAULT_BG_COLOR = 0
-FARAWAY = 1.0e39  # an implausibly huge distance
-
 
 class Cylinder:
     def __init__(self, position, direction, r, velocity):
@@ -16,6 +9,9 @@ class Cylinder:
         self.velocity = velocity
 
     def intersect(self, ray):
+        #функция, определяющая по геометрии лучей (начало и направление) и 
+        #по геометрии цилиндра (радиус, направление, точка, лежащая на оси цилиндра)
+        #пересекает ли луч цилиндр. Возвращает расстояние до ближайшего пересечения, если есть, иначе возвращает -1
         ray_3 = np.apply_along_axis(lambda x: x/np.linalg.norm(x), 1, np.delete(ray, 0, axis=1))
         oc = ray_3 - self.position
         card = np.sum(self.direction*ray_3, axis=1)
@@ -27,6 +23,7 @@ class Cylinder:
         return np.where(h < 0.0, -1, (-b - np.sqrt(h))/a)
 
     def boost_matrix(self):
+        #ищет матрицу преобразования Лоренца, взято из http://home.thep.lu.se/~malin/LectureNotesFYTA12_2016/SR6.pdf
         beta = np.asarray(self.velocity)
         beta_squared = np.inner(beta, beta)
         if beta_squared >= 1:
@@ -41,32 +38,20 @@ class Cylinder:
         return np.asarray(np.bmat([[lambda_00, lambda_0j], [lambda_i0, lambda_ij]]))
 
 
-(w, h) = (400, 300)
+(w, h) = (400, 300) #задаются ширина и высота снимка (в пикселях)
 
 x = np.tile(np.linspace(-1, 1, w), h)
 y = np.repeat(np.linspace(h/w, -h/w, h), w)
 focal_length = np.ones(h * w)
 ct = -np.sqrt(x ** 2 + y ** 2 + focal_length ** 2)
 
-rays = np.stack((ct, x, y, focal_length), axis=1)
+rays = np.stack((ct, x, y, focal_length), axis=1) #создает список лучей, заданных 4 координатами (ct, x, y, z)
 
-def raytrace(ray, obj):
-    return 0
+O = Cylinder(np.asarray([3.,2.,10.]), np.asarray([0.,1.,0.]), 0.2, 0.6) #содаем наш цилиндр
 
+boost_matrix = O.boost_matrix()#ищем матрицу преобразования Лоренца для перехода в систему отсчета цилиндра
 
-O = Cylinder(np.asarray([3.,2.,10.]), np.asarray([0.,1.,0.]), 0.2, 0.6)
-boost_matrix = O.boost_matrix()
-temp_rays = np.apply_along_axis(boost_matrix.dot, 1, rays) - boost_matrix.dot(ORIGIN)
-image = Image.fromarray(255*np.clip(O.intersect(temp_rays), 0, 1).reshape((h,w)).astype(np.uint8), mode="L")
+temp_rays = np.apply_along_axis(boost_matrix.dot, 1, rays) - boost_matrix.dot(np.array([0, 0, 0, 0]))#считаем координаты лучей, перейдя в систему отсчета цилиндра, используя np.apply_along_axis
+
+image = Image.fromarray(255*np.clip(O.intersect(temp_rays), 0, 1).reshape((h,w)).astype(np.uint8), mode="L")#используя O.intersect(temp_rays), ищем пересечения лучей с цилиндром
 image.save("final_image.png")
-scene = [O]
-
-image_data = []
-
-for obj in scene:
-    boost_matrix = obj.boost_matrix()
-    temp_rays = np.apply_along_axis(boost_matrix.dot, 1, rays) - boost_matrix.dot(ORIGIN)
-    image_data.append(raytrace(temp_rays, obj))
-
-a = np.asarray(([1.,2.,3.],[1.,2.,3.]))
-b = np.asarray(([1.,2.,3.],[1.,2.,3.]))
